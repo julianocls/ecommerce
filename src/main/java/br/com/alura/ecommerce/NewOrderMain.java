d9e90db54fafb523aaac179d27e03d35ad01b277
Juliano Santos <foxports@hotmail.com>
2022-01-21 14:39:12 -0300

    Cria novos consumidores para email e log

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -15,7 +16,7 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "23453425, 65787658, 9090909";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
+        Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
@@ -23,7 +24,14 @@ public class NewOrderMain {
                 System.out.println("sucesso enviando " + data.topic() + " // :::partition " + data.partition() +
                         " // offset " + data.offset() + " // timestamp " + data.timestamp());
             }
-        } ).get();
+        };
+
+        producer.send(record, callback).get();
+
+        var email = "Thank for your order! We are processing your order";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(emailRecord, callback).get();
+
     }
 
     private static Properties properties() {

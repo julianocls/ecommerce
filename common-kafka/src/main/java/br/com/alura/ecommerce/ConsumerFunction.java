8ca7235914de2558212d0a8397ab85cb2325d166
Juliano C L Santos <foxports@hotmail.com>
2022-01-26 16:11:19 -0300

    Rejeita pedidos com valor maior que 4500 e os considera como fraude. O FraudDetectorService publicara uma mensagem informado se foi aprovada ou rejetiada como fraude.

--- common-kafka/src/main/java/br/com/alura/ecommerce/ConsumerFunction.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/ConsumerFunction.java
@@ -2,8 +2,10 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.concurrent.ExecutionException;
+
 public interface ConsumerFunction<T> {
 
-    public void consume(ConsumerRecord<String, T> record);
+    public void consume(ConsumerRecord<String, T> record) throws ExecutionException, InterruptedException;
 
 }

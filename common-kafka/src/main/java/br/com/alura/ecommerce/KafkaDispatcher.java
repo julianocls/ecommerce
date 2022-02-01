96ecc31383451c28962b4fa41d8da6247e2da821
Juliano C L Santos <foxports@hotmail.com>
2022-02-01 10:24:01 -0300

    Refatoracao para remover o tipo do objeto que ficou desnecessario por causa da implementacao de Message<T>; Implementacao de metodo send() assincrono

--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
@@ -1,15 +1,13 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.Closeable;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 class KafkaDispatcher<T> implements Closeable {
 
@@ -24,23 +22,27 @@ class KafkaDispatcher<T> implements Closeable {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GSonSerializer.class.getName());
-        properties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, StringSerializer.class.getSimpleName() + "-" + UUID.randomUUID().toString());
+        properties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, StringSerializer.class.getSimpleName() + "-" + UUID.randomUUID());
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
         return properties;
     }
 
     void send(String topic, String key, CorrelationId id, T payload) throws ExecutionException, InterruptedException {
+        Future<RecordMetadata> future = sendAsync(topic, key, id, payload);
+        future.get();
+    }
+
+    Future<RecordMetadata> sendAsync(String topic, String key, CorrelationId id, T payload) {
         var value = new Message<>(id, payload);
         var record = new ProducerRecord<>(topic, key, value);
         Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
-                return;
             } else {
                 System.out.println("sucesso enviando " + data.topic() + " // :::partition " + data.partition() + " // offset " + data.offset() + " // timestamp " + data.timestamp());
             }
         };
-        producer.send(record, callback).get();
+        return producer.send(record, callback);
     }
 
     @Override
--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -10,7 +10,6 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Properties;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
 
 class KafkaService<T> implements Closeable {
@@ -18,19 +17,19 @@ class KafkaService<T> implements Closeable {
     private final KafkaConsumer<String, Message<T>> consumer;
     private final ConsumerFunction parse;
 
-    KafkaService(String groupId, String topic, ConsumerFunction<T> parse, Class<T> type, Map<String, String> properties) {
-        this(parse, groupId, type, properties);
+    KafkaService(String groupId, String topic, ConsumerFunction<T> parse, Map<String, String> properties) {
+        this(parse, groupId, properties);
         consumer.subscribe(Collections.singletonList(topic));
     }
 
-    public KafkaService(String groupId, Pattern topic, ConsumerFunction<T> parse, Class<T> type, Map<String, String> properties) {
-        this(parse, groupId, type, properties);
+    public KafkaService(String groupId, Pattern topic, ConsumerFunction<T> parse, Map<String, String> properties) {
+        this(parse, groupId, properties);
         consumer.subscribe(topic);
     }
 
-    private KafkaService(ConsumerFunction<T> parse, String groupId, Class<T> type, Map<String, String> properties) {
+    private KafkaService(ConsumerFunction<T> parse, String groupId, Map<String, String> properties) {
         this.parse = parse;
-        this.consumer = new KafkaConsumer<>(getProperties(type, groupId, properties));
+        this.consumer = new KafkaConsumer<>(getProperties(groupId, properties));
     }
 
     void run() {
@@ -51,14 +50,15 @@ class KafkaService<T> implements Closeable {
         }
     }
 
-    private Properties getProperties(Class<T> type, String grupoId, Map<String, String> overrideProperties) {
+    private Properties getProperties(String grupoId, Map<String, String> overrideProperties) {
         var properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG , grupoId);
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, grupoId);
         properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
         //properties.setProperty(GsonDeserializer.TYPE_CONFIG, type.getName()); // forma de serializar e deserializar altarda para utilizar Message<T>
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
         properties.putAll(overrideProperties);
         return properties;
     }
